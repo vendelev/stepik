6610e9ccb4bc4770b2738c8b52e2f5c7353953ee
vendelev <vend_arty@mail.ru>
2018-05-24 20:53:00 +0300

    1-2-4

--- 1-2-4.py
+++ 1-2-4.py
@@ -1,33 +1,58 @@
 import sys
 
-stack = []
-output= []
+stackMax = []
+stackVal = []
+output   = []
 
 def actionPop():
-    if len(stack) > 0:
-        stack.pop()
+    if len(stackVal) > 0:
+        stackMax.pop()
+        return stackVal.pop()
+    else:
+        return False
     
 def actionMax():
-    print(stack)
+    cnt = len(stackMax)
+    if cnt > 0:
+        return stackMax[cnt -1]
+    else:
+        return False
 
 def actionPush(value):
     data = value.split(' ')
-    stack.append(int(data[1]))
+    if data[0] != 'push':
+        return False
 
-countOperations = int(sys.stdin.readline())
+    value= int(data[1])
+    cnt  = len(stackMax)
 
-ii = 0
-while ii < countOperations:
-    command = sys.stdin.readline().strip()
+    if cnt > 0:
+        val = max(stackMax[cnt -1], value)
+    else:
+        val = value
 
+    stackMax.append(val)
+    stackVal.append(value)
+
+def printOutput():
+    for val in output:
+        print(val)
+
+def execCommand(command):
     if command == 'pop':
         actionPop()
     elif command == 'max':
-        actionMax()
+        val = actionMax()
+        if val:
+            output.append(val)
     else:
         actionPush(command)
-    
+
+ii = 0
+countOperations = int(sys.stdin.readline().strip())
+
+while ii < countOperations:
+    execCommand(sys.stdin.readline().strip())
     ii+=1
 
-for val in output:
-    print(val)
+printOutput()
