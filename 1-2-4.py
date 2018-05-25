15f8408614dc364519809598684936b0fcae58a5
vendelev <vend_arty@mail.ru>
2018-05-25 09:37:42 +0300

    1-2-4

--- 1-2-4.py
+++ 1-2-4.py
@@ -6,37 +6,42 @@ output   = []
 index    = 0
 
 def actionPop():
-    global index
+    global index, stackMax, stackVal
     if index > 0:
         index -= 1
-        stackMax.pop()
-        return stackVal.pop()
+        result = stackVal[index]
+        del stackMax[index]
+        del stackVal[index]
     else:
         return False
     
 def actionMax():
+    global index, stackMax, stackVal
     if index > 0:
-        return stackMax[index - 1]
+        val = stackMax[index - 1]
+        output.append(val)
+        return val
     else:
         return False
 
 def actionPush(value):
-    global index
+    global index, stackMax, stackVal
     data = value.split(' ')
     if data[0] != 'push':
         return False
 
-    value= int(data[1])
-    cnt  = len(stackMax)
+    value = int(data[1])
 
-    if cnt > 0:
-        val = max(stackMax[cnt -1], value)
+    if index > 0:
+        val = max(stackMax[index -1], value)
     else:
         val = value
 
     index += 1
     stackMax.append(val)
     stackVal.append(value)
+
+    return True
 
 def printOutput():
     for val in output:
@@ -46,17 +51,25 @@ def execCommand(command):
     if command == 'pop':
         actionPop()
     elif command == 'max':
-        val = actionMax()
-        if val:
-            output.append(val)
+        actionMax()
     else:
         actionPush(command)
 
-ii = 0
-countOperations = int(sys.stdin.readline().strip())
+# ii = 0
+# countOperations = int(sys.stdin.readline().strip())
 
-while ii < countOperations:
-    execCommand(sys.stdin.readline().strip())
-    ii+=1
+# while ii < countOperations:
+#     execCommand(sys.stdin.readline().strip())
+#     ii+=1
+
+operations = [
+    'push 5',
+    'pop',
+    'push 3',
+    'max'
+]
+
+for commnad in operations:
+    execCommand(commnad)
 
 printOutput()
