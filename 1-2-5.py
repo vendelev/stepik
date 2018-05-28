408004a4e01048fd9e58de3a8e059b77980d5fcc
vendelev <vend_arty@mail.ru>
2018-05-29 00:33:46 +0300

    1-2-5

--- 1-2-5.py
+++ 1-2-5.py
@@ -1,3 +1,5 @@
+import sys
+
 stack1 = []
 index1 = -1
 
@@ -13,10 +15,9 @@ def pop():
             index1 -= 1
             index2 += 1
             value = stack1.pop()
-            add_item(stack2, index2, value)
+            add_item(stack2, index2, value['item'])
 
-
-    if index2 == -1:
+    if index2 > -1:
         index2 -= 1
         return stack2.pop()
     else:
@@ -27,7 +28,7 @@ def push(value):
     global index1
 
     index1 += 1
-    add_item(stack1, index1, value)
+    add_item(stack1, index1, int(value))
 
 
 def add_item(stack, index, value):
@@ -43,15 +44,17 @@ def print_max():
     global index1, index2
 
     if index1 >= 0 and index2 >= 0:
-        print max(stack1[index1]['max'], stack2[index2]['max'])
-    elif index1 >= 0:
-        print stack1[index1]['max']
+        print(max(stack1[index1]['max'], stack2[index2]['max']))
     else:
-        print stack2[index2]['max']
+        if index1 >= 0:
+            print(stack1[index1]['max'])
+        else:
+            print(stack2[index2]['max'])
 
 
 def run(data, window):
     cnt = 0
+    window = int(window)
     for index, value in enumerate(data):
         cnt += 1
         push(value)
@@ -62,3 +65,12 @@ def run(data, window):
         if cnt > window:
             pop()
             print_max()
+
+
+sys.stdin.readline().strip()
+run(sys.stdin.readline().strip().split(' '), sys.stdin.readline().strip())
+
+
+# run('2 7 3 1 5 2 6 2'.split(' '), 4)
+# run('2 1 5'.split(' '), 1)
+# run('2 3 9'.split(' '), 3)
